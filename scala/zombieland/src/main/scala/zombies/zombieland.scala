0037a5d5c320a93f66ba1472d32c538db95a2f7b
Clément Caron <caron.clement@gmail.com>
2019-09-19 16:02:11 +0200

    Initial Commit

---
+++ scala/zombieland/src/main/scala/zombies/zombieland.scala
@@ -0,0 +1,58 @@
+package zombies
+
+import zombies.guitutils.parameters._
+import scala.scalajs.js.annotation.JSExportTopLevel
+
+object zombieland {
+  @JSExportTopLevel("zombies")
+  def zombies(): Unit = {
+    simulate.buildGUI(
+      (_, _) => simulation.environment.quarantine,
+      infectionRange,
+      walkSpeed,
+
+      walkNoise,
+      runNoise,
+      exhaustNoise,
+      fightNoise,
+
+      humanMaxRotation,
+      humanPerception,
+      humanExhaustionProbability,
+      humanFightBackProbability,
+      humanRunSpeed,
+      humanFollowProbability,
+      humanInformProbability,
+      humanInformedRatio,
+      numberHumans,
+
+      zombieMaxRotation,
+      zombiePerception,
+      zombieRunSpeed,
+      zombiePheromoneEvaporation,
+      numberZombies,
+
+      armyOnOff,
+      armySize,
+      armyFightBackProbability,
+      armyExhaustionProbability,
+      armyPerception,
+      armyRunSpeed,
+      armyFollowProbability,
+      armyMaxRotation,
+      armyInformProbability,
+      armyAggressive,
+
+      redCrossOnOff,
+      redCrossSize,
+      redCrossExhaustionMechanism,
+      redCrossExhaustionProbability,
+      redCrossFollowProbability,
+      redCrossInformProbability,
+      redCrossAggressive,
+      activationDelay,
+      efficiencyProbability
+    )
+  }
+
+}
